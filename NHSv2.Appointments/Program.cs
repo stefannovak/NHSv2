12c84ef6b529d1d4cdf2bb7762fe86cf71c51381
Stefan Novak <stefannovak96@gmail.com>
2024-12-07 22:27:01 +0000

    feat: basic Keycloak auth and ocelot auth

--- NHSv2.Appointments/Program.cs
+++ NHSv2.Appointments/Program.cs
@@ -1,9 +1,29 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    .AddAuthentication("Keycloak")
+    .AddJwtBearer("Keycloak", options =>
+    {
+        options.Authority = "http://localhost:8080/realms/NHSv2-doctors-dev";
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.RequireHttpsMetadata = false;
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("RequireAuth", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+    });
+});
 
 var app = builder.Build();
 
@@ -14,6 +34,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseHttpsRedirection();
 
 var summaries = new[]
@@ -21,7 +43,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/appointments", () =>
+app.MapGet("/appointments", (ClaimsPrincipal claimsPrincipal) =>
     {
         var forecast = Enumerable.Range(1, 5).Select(index =>
                 new WeatherForecast
@@ -34,7 +56,8 @@ app.MapGet("/appointments", () =>
         return forecast;
     })
     .WithName("GetWeatherForecast")
-    .WithOpenApi();
+    .WithOpenApi()
+    .RequireAuthorization("RequireAuth");
 
 app.Run();
 
--- NHSv2.Gateway/Program.cs
+++ NHSv2.Gateway/Program.cs
@@ -4,12 +4,20 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services
+    .AddAuthentication("Keycloak")
+    .AddJwtBearer("Keycloak", options =>
+    {
+        options.Authority = "http://localhost:8080/realms/NHSv2-doctors-dev";
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.RequireHttpsMetadata = false;
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
@@ -20,6 +28,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 await app.UseOcelot();
+app.UseHttpsRedirection();
 app.Run();
