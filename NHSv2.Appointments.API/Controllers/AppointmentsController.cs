3ac815e463ea43dc3b790ba04fb563f5a6ef205e
Stefan Novak <stefannovak96@gmail.com>
2024-12-14 23:25:50 +0000

    feat: eventstoredb

---
+++ NHSv2.Appointments.API/Controllers/AppointmentsController.cs
@@ -0,0 +1,39 @@
+using System.Text.Json;
+using EventStore.Client;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NHSv2.Appointments.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AppointmentsController(EventStoreClient eventStoreClient) : ControllerBase
+{
+    [Authorize(Roles = "patient")]
+    [HttpGet("/appointments")]
+    public async Task<ActionResult> Get()
+    {
+        for (var i = 0; i < 100; i++)
+        {
+            var evt = new {
+                EntityId      = Guid.NewGuid().ToString("N"),
+                ImportantData = $"I wrote my first event! {i}"
+            };
+
+            var eventData = new EventData(
+                Uuid.NewUuid(),
+                "TestEvent",
+                JsonSerializer.SerializeToUtf8Bytes(evt)
+            );
+            
+            await eventStoreClient.AppendToStreamAsync(
+                "some-stream",
+                StreamState.Any,
+                new[] { eventData },
+                cancellationToken: new CancellationToken()
+            );
+        }
+        
+        return Ok();
+    }
+}
---
+++ NHSv2.Appointments.API/Program.cs
@@ -0,0 +1,65 @@
+using EventStore.Client;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services
+    .AddAuthentication("Keycloak")
+    .AddJwtBearer("Keycloak", options =>
+    {
+        options.Authority = "http://localhost:8080/realms/NHSv2-dev";
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.RequireHttpsMetadata = false;
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Patient", policy =>
+    {
+        policy.RequireRole("patient");
+    });
+    
+    options.AddPolicy("Doctor", policy =>
+    {
+        policy.RequireRole("doctor");
+    });
+});
+
+const string connectionString = "esdb://localhost:2113?tls=false&tlsVerifyCert=false";
+var settings = EventStoreClientSettings.Create(connectionString);
+var eventStoreClient = new EventStoreClient(settings);
+builder.Services.AddSingleton(eventStoreClient);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+// app.MapGet("/appointments", () => Results.Ok())
+//     .WithName("Patient")
+//     .WithOpenApi()
+//     .RequireAuthorization("Patient");
+//
+//
+// app.MapGet("/appointments-doctor", () => Results.Ok())
+//     .WithName("Doctor")
+//     .WithOpenApi()
+//     .RequireAuthorization("Doctor");
+
+app.Run();
