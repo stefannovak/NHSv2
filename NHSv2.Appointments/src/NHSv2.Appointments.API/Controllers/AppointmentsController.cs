5a776be4e1d586a9d2463668c7288d2d6cc1aab6
Stefan Novak <stefannovak96@gmail.com>
2025-01-28 22:31:48 +0000

    feat: appointments endpoint and dummy event data

--- NHSv2.Appointments/src/NHSv2.Appointments.API/Controllers/AppointmentsController.cs
+++ NHSv2.Appointments/src/NHSv2.Appointments.API/Controllers/AppointmentsController.cs
@@ -30,24 +30,33 @@ public class AppointmentsController : ControllerBase
     [HttpPost]
     public async Task<ActionResult> CreateAppointment([FromBody] CreateAppointmentRequestDto request)
     {
+        var doctorName = User.Claims.FirstOrDefault(c => c.Type == "preferred_username")?.Value;
+        var doctorEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+        var facilitiesDoctorBelongsTo = User.Claims.Where(c => c.Type == "medical_facilities");
+        var doctorBelongsToRequestFacility = facilitiesDoctorBelongsTo.Any(x => x.Value == request.FacilityName);
+        if (doctorName == null || doctorEmail == null)
+        {
+            return Problem("Doctor name or email not found could not be found from token.");
+        }
+        
+        if (!doctorBelongsToRequestFacility)
+        {
+            return Unauthorized("Doctor does not belong to the facility.");
+        }   
+        
         var isAppointmentSlotAvailable = await _calendarService.IsAppointmentSlotAvailable(request.Start);
         if (!isAppointmentSlotAvailable)
         {
             return BadRequest("Appointment slot is not available. Ensure there is 30 minutes between appointments.");
         }
         
-        var doctorName = User.Claims.FirstOrDefault(c => c.Type == "preferred_username")?.Value;
-        var doctorEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
-        if (doctorName == null || doctorEmail == null)
-        {
-            return Problem("Doctor name, email or clinic not found could not be found from token.");
-        }
-        
         var appointmentCreatedCommand = new CreateAppointmentCommand(
             request.Start,
             request.End,
             request.PatientId,
-            request.Description);
+            request.Summary,
+            request.Description,
+            request.FacilityName);
         
         await _mediator.Send(appointmentCreatedCommand);
         return Accepted();
--- NHSv2.Appointments/src/NHSv2.Appointments.Application/Appointments/Commands/CreateAppointment/CreateAppointmentCommand.cs
+++ NHSv2.Appointments/src/NHSv2.Appointments.Application/Appointments/Commands/CreateAppointment/CreateAppointmentCommand.cs
@@ -8,11 +8,13 @@ namespace NHSv2.Appointments.Application.Appointments.Commands.CreateAppointment
 /// <param name="Start"></param>
 /// <param name="End"></param>
 /// <param name="PatientId"></param>
+/// <param name="Summary"></param>
 /// <param name="Description"></param>
-/// <param name="clinicId">To come in v2.</param>
+/// <param name="facilityName">To come in v2.</param>
 public record CreateAppointmentCommand(
     DateTime Start,
     DateTime End,
     Guid PatientId,
+    string Summary,
     string Description,
-    Guid clinicId = default) : IRequest;
+    string facilityName) : IRequest;
