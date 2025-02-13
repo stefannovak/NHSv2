241538b997e679830bc8c3350a36b642657de26d
Stefan Novak - Laptop <stefannovak96@gmail.com>
2025-02-13 22:08:17 +0000

    refactor: remove unused json property names

--- NHSv2.Appointments/src/NHSv2.Appointments.Application/Models/KeycloakUser.cs
+++ NHSv2.Appointments/src/NHSv2.Appointments.Application/Models/KeycloakUser.cs
@@ -1,5 +1,3 @@
-using System.Text.Json.Serialization;
-
 namespace NHSv2.Appointments.Application.Models;
 
 public class KeycloakUser
@@ -10,9 +8,7 @@ public class KeycloakUser
     
     public string Email { get; set; } = string.Empty;
     
-    // [JsonPropertyName("firstName")]
     public string FirstName { get; set; } = string.Empty;
     
-    // [JsonPropertyName("lastName")]
     public string LastName { get; set; } = string.Empty;
 }
