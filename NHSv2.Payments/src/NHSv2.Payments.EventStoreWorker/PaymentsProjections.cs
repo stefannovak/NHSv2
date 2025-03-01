9bb7ebace1ad2a83e84c1fbfd6afa536dfa1de14
Stefan Novak - Laptop <stefannovak96@gmail.com>
2025-03-01 15:54:26 +0000

    feat: projections

--- NHSv2.Payments/src/NHSv2.Payments.EventStoreWorker/PaymentsProjections.cs
+++ NHSv2.Payments/src/NHSv2.Payments.EventStoreWorker/PaymentsProjections.cs
@@ -5,6 +5,7 @@ using NHSv2.Payments.Application.Helpers;
 using NHSv2.Payments.Application.Repositories;
 using NHSv2.Payments.Domain.Transactions;
 using NHSv2.Payments.Domain.Transactions.Payments.Events;
+using NHSv2.Payments.Domain.Transactions.Payments.Events.Stripe;
 
 namespace NHSv2.Payments.EventStoreWorker;
 
@@ -57,6 +58,30 @@ public class PaymentsProjections : BackgroundService
             case nameof(PaymentCreatedEvent):
                 await HandlePaymentCreated(evnt);
                 break;
+            case nameof(PaymentIntentCanceledEvent):
+                await HandlePaymentIntentCanceled(evnt);
+                break;
+            case nameof(PaymentIntentCreatedEvent):
+                await HandlePaymentIntentCreated(evnt);
+                break;
+            case nameof(PaymentIntentPartiallyFundedEvent):
+                await HandlePaymentIntentPartiallyFunded(evnt);
+                break;
+            case nameof(PaymentIntentPaymentFailedEvent):
+                await HandlePaymentIntentPaymentFailed(evnt);
+                break;
+            case nameof(PaymentIntentProcessingEvent):
+                await HandlePaymentIntentProcessing(evnt);
+                break;
+            case nameof(PaymentIntentRequiresActionEvent):
+                await HandlePaymentIntentRequiresAction(evnt);
+                break;
+            case nameof(PaymentIntentSucceededEvent):
+                await HandlePaymentIntentSucceeded(evnt);
+                break;
+            default:
+                Console.WriteLine($"Failed to handle event {evnt.Event.EventType}");
+                break;
         }
         
         await _checkpointRepository.IncrementCheckpoint(StreamName);
@@ -74,17 +99,72 @@ public class PaymentsProjections : BackgroundService
 
         activity?.AddTag(nameof(paymentCreatedEvent.TransactionId), paymentCreatedEvent.TransactionId);
         await InsertPaymentToDatabase(paymentCreatedEvent);
-        _logger.LogInformation($"Handling payment created: {paymentCreatedEvent.TransactionId}");
     }
     
+    private Task HandlePaymentIntentCanceled(ResolvedEvent evnt)
+    {
+        return HandlePaymentIntentEvent<PaymentIntentCanceledEvent>(evnt, TransactionStatus.Canceled);
+    }
+    
+    private Task HandlePaymentIntentCreated(ResolvedEvent evnt)
+    {
+        return HandlePaymentIntentEvent<PaymentIntentCreatedEvent>(evnt, TransactionStatus.Pending);
+    }
+    
+    private Task HandlePaymentIntentPartiallyFunded(ResolvedEvent evnt)
+    {
+        return HandlePaymentIntentEvent<PaymentIntentPartiallyFundedEvent>(evnt, TransactionStatus.RequiresAction);
+    }
+    
+    private Task HandlePaymentIntentPaymentFailed(ResolvedEvent evnt)
+    {
+        return HandlePaymentIntentEvent<PaymentIntentPaymentFailedEvent>(evnt, TransactionStatus.Failed);
+    }
+    
+    private Task HandlePaymentIntentProcessing(ResolvedEvent evnt)
+    {
+        return HandlePaymentIntentEvent<PaymentIntentProcessingEvent>(evnt, TransactionStatus.Pending);
+    }
+    
+    private Task HandlePaymentIntentRequiresAction(ResolvedEvent evnt)
+    {
+        return HandlePaymentIntentEvent<PaymentIntentRequiresActionEvent>(evnt, TransactionStatus.RequiresAction);
+    }
+    
+    private Task HandlePaymentIntentSucceeded(ResolvedEvent evnt)
+    {
+        return HandlePaymentIntentEvent<PaymentIntentSucceededEvent>(evnt, TransactionStatus.Success);
+    }
+
+    private async Task HandlePaymentIntentEvent<T>(ResolvedEvent evnt, TransactionStatus status) where T : PaymentIntentBaseEvent
+    {
+        using var activity = ActivitySourceHelper.ActivitySource.StartActivity();
+        var piEvent = JsonSerializer.Deserialize<T>(evnt.Event.Data.Span);
+        if (piEvent == null)
+        {
+            activity?.LogExceptionEvent("FailedToDeserialize", new ArgumentException(), nameof(evnt));
+            return;
+        }
+
+        activity?.AddTag(nameof(piEvent.TransactionId), piEvent.TransactionId);
+        var payments = _paymentsRepository.GetPayments(piEvent.TransactionId).ToList();
+        foreach (var payment in payments)
+        {
+            // TODO: - Properly handle events arriving out of order.
+            if (payment.Status != TransactionStatus.Success)
+            {
+                payment.Status = status;
+                await _paymentsRepository.UpdateAsync(payment);
+            }
+        }
+    }
+
     private async Task InsertPaymentToDatabase(PaymentCreatedEvent createdEvent)
     {
-        var paymentTuples = Payment.CreatePaymentsFromProducts(createdEvent.Products);
-        foreach (var payment in paymentTuples.payments)
+        foreach (var product in createdEvent.Products)
         {
+            var payment = new Payment(product, createdEvent.TransactionId);
             await _paymentsRepository.InsertAsync(payment);
         }
-        
-        _logger.LogInformation($"Inserted payments {paymentTuples.transactionId} to database");
     }
 }
